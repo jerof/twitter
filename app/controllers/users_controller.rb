3574164037b98b2f39b07a19a9e02be9bc8d437b
Nikhil <nikhilsamuel.89@gmail.com>
2018-09-28 17:14:21 -0400

    Add signin upon signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ before_action :require_same_user, only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Twitter, #{@user.username}!"
       redirect_to tweets_path
     else
